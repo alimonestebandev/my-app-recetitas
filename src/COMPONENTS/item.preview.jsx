3b55588309c22000f7e376a69f3f040452362cd2
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-02 18:47:10 -0300

    se agrega informacion adicional al componente: createdAt y author

--- src/COMPONENTS/item.preview.jsx
+++ src/COMPONENTS/item.preview.jsx
@@ -1,4 +1,6 @@
 function ItemPreview({
+  createdAt = "undefined",
+  author = "null",
   title = "undefined",
   description = "undefined",
   urlImg = "https://uxwing.com/wp-content/themes/uxwing/download/food-and-drinks/food-dinner-icon.png",
@@ -10,7 +12,12 @@ function ItemPreview({
       </div>
       <div className="data">
         <h3>{title}</h3>
+
         <p>{description}</p>
+      </div>
+      <div className="data-time">
+        <p>Creado por: {author || "undefined"} |</p>
+        <p>Fecha: {createdAt}</p>
       </div>
     </button>
   );
