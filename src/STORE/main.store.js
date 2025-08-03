9cf971ae8c8b09701f7fd196a25688c899bc0710
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-03 16:46:02 -0300

    funcion setItemsStore

--- src/STORE/main.store.js
+++ src/STORE/main.store.js
@@ -3,6 +3,7 @@ import { create } from "zustand";
 const store = create((set) => ({
   items: "",
   recipeSelected: "",
+  setItemsStore: (newItems) => set({ items: newItems }),
   setSelectedRecipe: (newRecipe) => set({ recipeSelected: newRecipe }),
 }));
 
