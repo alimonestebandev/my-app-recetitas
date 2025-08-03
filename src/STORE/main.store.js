e08545253fe9f43465cccc04e3e8799ecfdcd0df
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-03 15:35:23 -0300

    se agrega variable global para contener todos los items(recetas)

--- src/STORE/main.store.js
+++ src/STORE/main.store.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 
 const store = create((set) => ({
+  items: "",
   recipeSelected: "",
   setSelectedRecipe: (newRecipe) => set({ recipeSelected: newRecipe }),
 }));
