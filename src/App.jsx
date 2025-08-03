59bcb503e1ca10b77325a3b31cfde15344c3c707
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-03 13:55:58 -0300

    se agrega la ruta /see-recipe y el elemento completeRecipe

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./PAGES/main.jsx";
+import CompleteRecipe from "./PAGES/complete-recipe.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        <Route path="/see-recipe" element={<CompleteRecipe />}></Route>
       </Routes>
     </BrowserRouter>
   );
