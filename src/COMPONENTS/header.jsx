40038d9080c24e6fe5e2ac92dcd21917552f4d63
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-03 15:33:31 -0300

    creacion del boton para crear receta

--- src/COMPONENTS/header.jsx
+++ src/COMPONENTS/header.jsx
@@ -1,5 +1,7 @@
 import SearchIcon from "@mui/icons-material/Search";
 import BreakfastDiningIcon from "@mui/icons-material/BreakfastDining";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -11,9 +13,16 @@ function Header() {
       </div>
       <div className="search-container">
         <input className="input-search" type="text" />
-        <button title="Buscar" className="btn-search">
+        <button title="Buscar" className="btn-searchbar">
           <SearchIcon></SearchIcon>
         </button>
+        <Link
+          to="/create-recipe"
+          title="Crear publicación"
+          className="btn-searchbar"
+        >
+          <AddBoxIcon /> Publicar
+        </Link>
       </div>
     </header>
   );
