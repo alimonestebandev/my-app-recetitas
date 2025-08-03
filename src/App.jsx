cd7c025926fdf2d401563d1f637405dde02ca126
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-03 15:32:06 -0300

    creacion de pagina para crear receta

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./PAGES/main.jsx";
 import CompleteRecipe from "./PAGES/complete-recipe.jsx";
+import CreateRecipe from "./PAGES/create-recipe.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}></Route>
-        <Route path="/see-recipe" element={<CompleteRecipe />}></Route>
+        <Route path="/see-recipe" element={<CompleteRecipe />} />
+        <Route path="/create-recipe" element={<CreateRecipe />} />
       </Routes>
     </BrowserRouter>
   );
