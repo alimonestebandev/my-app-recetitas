535677e7b42d953a9b178fd9b8e695102fd14f1f
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-03 16:46:45 -0300

    integracion de errores de formulario y funcion para añadir a la lista de items un nuevo item

--- src/pages/create-recipe.jsx
+++ src/pages/create-recipe.jsx
@@ -1,7 +1,41 @@
 import BackHomeBtn from "../COMPONENTS/backhome-btn";
 import "../CSS/create-recipe.css";
+import { useForm } from "react-hook-form";
+
+import store from "../STORE/main.store.js";
 
 function CreateRecipe() {
+  const { items, setItemsStore } = store();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitForm = (values) => {
+    const {
+      title,
+      description,
+      urlImg,
+      instructions,
+      ingredients,
+      author,
+    } = values;
+
+    //Crea un item y lo añade a la lista de items global
+
+    const newObj = {
+      id: items.length + 1,
+      title,
+      description,
+      author,
+      urlImg,
+      instructions,
+      ingredients,
+    };
+    setItemsStore([newObj, ...items]);
+  };
+
   return (
     <div className="central-container">
       <BackHomeBtn></BackHomeBtn>
@@ -9,19 +43,76 @@ function CreateRecipe() {
         Creá una nueva Receta
       </h3>
       <div>
-        <form className="form-create-recipe" action="">
-          <h4>URL image</h4>
-          <input type="text" name="img" />
-          <h4>Titulo *</h4>
-          <input type="text" />
-          <h4>Breve descripcion *</h4>
-          <input type="text" />
-          <h4>Autor *</h4>
-          <input type="text" />
-          <h4>Ingredientes</h4>
-          <textarea resize="none" type="text" />
+        <form
+          onSubmit={handleSubmit(onSubmitForm)}
+          className="form-create-recipe"
+          action=""
+        >
+          <h4>Direccion URL de Imagen (Opcional)</h4>
+          <input
+            {...register("urlImg", { required: false, minLength: 5 })}
+            type="text"
+          />
+
+          <h4>Título *</h4>
+          <p style={{ margin: "0", color: "red" }}>{errors?.title?.message}</p>
+          <input
+            {...register("title", {
+              required: "Ingresá el título, campo obligatorio *",
+              minLength: 5,
+            })}
+          />
+
+          <h4>Breve descripción (Max 100 caracteres) *</h4>
+          <p style={{ margin: "0", color: "red" }}>
+            {errors?.description?.message}
+          </p>
+          <input
+            {...register("description", {
+              required: "Ingresá una breve descripción, campo obligatorio *",
+              minLength: 5,
+              maxLength: {
+                value: 100,
+                message:
+                  "Superaste los caracteres máximos, deben ser menor a 100 ",
+              },
+            })}
+            type="text"
+          />
+
+          <h4>Autor (Max 30 caracteres) *</h4>
+          <p style={{ margin: "0", color: "red" }}>{errors?.author?.message}</p>
+          <input
+            {...register("author", {
+              required: "Ingresá tu nombre o del autor, campo obligatorio *",
+              minLength: 3,
+              maxLength: {
+                value: 30,
+                message:
+                  "Superaste los caracteres máximos, deben ser menor a 30",
+              },
+            })}
+            type="text"
+          />
+
+          <h4>Ingredientes (Opcional)</h4>
+          <textarea
+            {...register("ingredients", {
+              minLength: 1,
+            })}
+            type="text"
+          />
           <h4>Instrucciones *</h4>
-          <textarea resize={false} type="text" />
+          <p style={{ margin: "0", color: "red" }}>
+            {errors?.instructions?.message}
+          </p>
+          <textarea
+            {...register("instructions", {
+              required: "Ingresá las instrucciones, campo obligatorio *",
+              minLength: 5,
+            })}
+            type="text"
+          />
 
           <input className="btn-post" type="submit" value="Publicar" />
           <p>(*) Campos obligatorios</p>
