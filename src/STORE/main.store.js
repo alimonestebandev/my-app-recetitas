b104e601042403865e080d16a58a74156ff78229
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-06 00:33:04 -0300

    el main store almacena la opcion seleccionada optionSelected

--- src/STORE/main.store.js
+++ src/STORE/main.store.js
@@ -2,8 +2,10 @@ import { create } from "zustand";
 
 const store = create((set) => ({
   items: "",
+  optionSelected: 0,
   recipeSelected: "",
   setItemsStore: (newItems) => set({ items: newItems }),
+  setOptionSelected: (newOption) => set({ optionSelected: newOption }),
   setSelectedRecipe: (newRecipe) => set({ recipeSelected: newRecipe }),
 }));
 
