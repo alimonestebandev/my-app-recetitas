d337e3cc12c70afc1a944387be2f1df855807b31
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-06 00:30:16 -0300

    el componente puede marcar como favorita la receta que contiene

--- src/COMPONENTS/item.preview.jsx
+++ src/COMPONENTS/item.preview.jsx
@@ -1,22 +1,37 @@
 import store from "../STORE/main.store";
 import { Link } from "react-router-dom";
-function ItemPreview({ item }) {
-  const { title, description, author, createdAt, urlImg } = item;
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
-  const { setSelectedRecipe } = store();
+function ItemPreview({ item }) {
+  const {
+    id,
+    apiItem,
+    title,
+    description,
+    author,
+    createdAt,
+    urlImg,
+    isFavorite,
+  } = item;
+
+  const { setSelectedRecipe, items, setItemsStore } = store();
+
+  const setFavorite = () => {
+    var item = items.find((e) => e.id == id);
+    item.isFavorite = !item.isFavorite;
+    setItemsStore(items);
+
+    localStorage.setItem("LOCAL-DATA", JSON.stringify(items));
+  };
 
   const selectRecipe = () => {
     setSelectedRecipe(item);
   };
 
   return (
-    <Link
-      to="/see-recipe"
-      onClick={() => selectRecipe()}
-      title="ver más"
-      className="item-preview"
-    >
-      <div>
+    <div title="ver más" className="item-preview">
+      <Link to="/see-recipe" onClick={() => selectRecipe()} className="data">
         <img
           className="image-preview"
           src={
@@ -25,17 +40,25 @@ function ItemPreview({ item }) {
           }
           alt=""
         />
-      </div>
-      <div className="data">
-        <h3>{title}</h3>
-
-        <p>{description}</p>
-      </div>
+        <div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+      </Link>
+      {!apiItem && (
+        <button onClick={() => setFavorite()} className="favorite-icon">
+          {isFavorite ? (
+            <FavoriteIcon></FavoriteIcon>
+          ) : (
+            <FavoriteBorderIcon></FavoriteBorderIcon>
+          )}
+        </button>
+      )}
       <div className="data-time">
         <p>Creado por: {author || "undefined"}</p>
         {createdAt && <p>| Fecha: {createdAt}</p>}
       </div>
-    </Link>
+    </div>
   );
 }
 
