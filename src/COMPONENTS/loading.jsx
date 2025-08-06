438bdfa27008e1dd035ef6b4b314b36f1ce4d478
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-06 02:30:59 -0300

    nuevo diseño circulo de carga

--- src/COMPONENTS/loading.jsx
+++ src/COMPONENTS/loading.jsx
@@ -1,12 +1,7 @@
-import BreakfastDiningIcon from "@mui/icons-material/BreakfastDining";
-
 function Loading() {
   return (
     <div>
-      <div className="loading-circle">
-        <BreakfastDiningIcon></BreakfastDiningIcon>
-        Cargando...
-      </div>
+      <div className="loading-circle"></div>
     </div>
   );
 }
