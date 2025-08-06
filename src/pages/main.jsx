661f05d186ddf7206da1a90abcdff448b946a854
alimonestebandev <alimonestebanfacundo@gmail.com>
2025-08-06 00:38:01 -0300

    funcionalidad de carga de datos API y datos Locales, funcionalidad de Filtros Favoritos funcionales

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -10,12 +10,20 @@ import BreakfastDiningIcon from "@mui/icons-material/BreakfastDining";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import { Link } from "react-router-dom";
 import Footer from "../COMPONENTS/footer.jsx";
-
+import CoffeeIcon from "@mui/icons-material/Coffee";
 function Main() {
   const { state, changeState } = useStateLoading();
-  const { setRecipeSelected, setItemsStore, items } = store();
+  const {
+    setRecipeSelected,
+    setItemsStore,
+    items,
+    setOptionSelected,
+    optionSelected,
+  } = store();
 
-  const [filteredItems, setFilteredItems] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const [filteredItems, setFilteredItems] = useState([]);
 
   // const invertItemsList = () => {
   //   console.log(filteredItems);
@@ -27,6 +35,24 @@ function Main() {
   // };
 
   // useEffect(() => invertItemsList, [setFilteredItems]);
+
+  useEffect(() => applyFilter(), [items]);
+
+  const applyFilter = () => {
+    var option = document.getElementById("filter_option");
+    if (option?.value == "0") {
+      setFilteredItems(null);
+    } else if (option?.value == "1") {
+      showOnlyFavorites();
+    }
+  };
+
+  const showOnlyFavorites = () => {
+    if (items) {
+      var newItems = items.filter((item) => item.isFavorite);
+      setFilteredItems(newItems);
+    }
+  };
 
   const searchItem = (words) => {
     if (words.target.value.length > 2) {
@@ -50,31 +76,47 @@ function Main() {
     }
   };
 
-  const getDATA = () => {
+  const getLocalDATA = () => {
+    setOptionSelected(1);
     if (localStorage.getItem("LOCAL-DATA")) {
       var LocalData = JSON.parse(localStorage.getItem("LOCAL-DATA"));
+      setFilteredItems(LocalData);
       setItemsStore(LocalData);
+    } else {
       setFilteredItems(null);
-      // setFilteredItems(LocalData);
-      changeState();
-      return setItemsStore(LocalData);
+      setItemsStore(null);
     }
+    if (state == true) {
+      changeState();
+    }
+  };
 
+  const getApiDATA = () => {
+    setOptionSelected(0);
+    setItemsStore(null);
+    setFilteredItems(null);
     fetch("src/DATA/api.json")
       .then((res) => res.json())
       .then((data) => {
-        if (items <= 0) {
+        if (data.length >= 1) {
           setItemsStore(data);
           setFilteredItems(data);
-        } else {
-          setFilteredItems(items);
+          setErrorMsg(null);
         }
-        changeState();
       })
-      .catch((error) => console.error("Error al obtener el JSON:", error));
+      .catch((error) => {
+        setErrorMsg("Error al cargar publicaciones");
+      });
+    if (state == true) {
+      changeState();
+    }
   };
   useEffect(() => {
-    getDATA();
+    if (optionSelected == 0) {
+      getApiDATA();
+    } else if (optionSelected == 1) {
+      getLocalDATA();
+    }
   }, []);
 
   return (
@@ -90,17 +132,41 @@ function Main() {
           <button title="Buscar" className="btn-searchbar">
             <SearchIcon></SearchIcon>
           </button>
-          <Link
-            to="/create-recipe"
-            title="Crear publicación"
-            className="btn-searchbar"
-          >
-            <AddBoxIcon /> Publicar
-          </Link>
         </div>
+        <div className="header-btns">
+          <button
+            className={optionSelected == 0 ? "btn-selected" : " "}
+            onClick={getApiDATA}
+          >
+            Novedades
+          </button>
+          <button
+            className={optionSelected == 1 ? "btn-selected" : " "}
+            onClick={getLocalDATA}
+          >
+            Mis Recetas
+          </button>
+        </div>
+        {optionSelected == 1 && (
+          <div className="my-recipes-bar">
+            <h4>Mostrar:</h4>
+            <select id="filter_option" onChange={applyFilter}>
+              <option value="0">Todos</option>
+              <option value="1">Favoritos</option>
+            </select>
+            <Link
+              to="/create-recipe"
+              title="Crear Receta"
+              className="btn-create"
+            >
+              <AddBoxIcon />
+              <p>Crear Receta</p>
+            </Link>
+          </div>
+        )}
       </header>
       <main>
-        <div
+        {/* <div
           style={{
             paddingTop: "5px",
             borderTop: "1px solid black",
@@ -108,14 +174,14 @@ function Main() {
             height: "40px",
           }}
         >
-          {/* <button
+          <button
             title="Invertir Lista"
             className="filter-btn-arrows"
             onClick={invertItemsList}
           >
             <SwapVertIcon></SwapVertIcon>
-          </button> */}
-        </div>
+          </button>
+        </div> */}
         {!state && filteredItems == null
           ? items?.map((e, index) => {
               return (
@@ -138,7 +204,22 @@ function Main() {
               );
             })}
         {state && <Loading />}
+        {errorMsg && optionSelected == 0 && (
+          <h4 style={{ color: "red" }}>{errorMsg}</h4>
+        )}
+        {(items?.length <= 0 && filteredItems?.length <= 0) ||
+          (items?.length >= 0 && filteredItems?.length <= 0) ||
+          items == [] ||
+          (items == null && (
+            <div style={{ color: "gray" }}>
+              <CoffeeIcon></CoffeeIcon>
+              <h4 style={{ margin: "0", color: "gray", fontWeight: "normal" }}>
+                No hay Recetas para mostrar.
+              </h4>
+            </div>
+          ))}
       </main>
+
       <Footer></Footer>
     </div>
   );
